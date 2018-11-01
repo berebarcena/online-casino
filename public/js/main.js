438f841778af1d366efeccb21088ad4a395f0541
Berenice <berebarcenarm@gmail.com>
2018-11-01 22:21:16 +0100

    added button triggers for settings

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
   let numSlots = 3;
-  let skin = 'pusheen';
+  let skin = 'mario';
   const userId = parseInt($('#userId').val());
 
   let slotMachine1 = new SlotMachine(userId, numSlots, skin);
@@ -8,9 +8,29 @@ $(function() {
   $('#play').on('click', function() {
     slotMachine1.start();
   });
-  $('#test').on('click', function() {
-    numSlots = 4;
-    slotMachine1.reload(skin, numSlots);
-    slotMachine1._setLevel();
+
+  $('.trigger-modal').on('click', function() {
+    if ($(this).data('target') === 'skin') {
+      $('.skin').addClass('open');
+    } else {
+      $('.bet').addClass('open');
+    }
+  });
+
+  $('.settings').on('click', function() {
+    if ($(this).data('skin')) {
+      skin = $(this).data('skin');
+      slotMachine1.reload(skin, numSlots);
+      $('.skin').removeClass('open');
+    } else {
+      numSlots = parseInt($(this).data('level'));
+      slotMachine1.reload(skin, numSlots);
+      slotMachine1._setLevel();
+      $('.bet').removeClass('open');
+    }
+  });
+
+  $('.close').on('click', function() {
+    $('.modal').removeClass('open');
   });
 });
